ed7f52639428f674157d8b742bcd5642f61e3fbf
lindajoy <wawirajoy98@gmail.com>
2022-09-22 18:02:49 +0300

    ✏ Valid Palindrome 2 Optimal solutions

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -3,18 +3,32 @@ Valid Palindrome: https://leetcode.com/problems/valid-palindrome/
 """
 
 """
-First attempt: Works with strings with no punctuation marks😊
+💡 First attempt: Works with strings with no punctuation marks😊
 """
 def is_palindrome(str):
-    a = str
-    print(a)
     b = ''.join(reversed(str))
     print(b)
-    if a == b:
+    if str == b:
        return True
     else:
         return False
 
 print(is_palindrome('mom'))
 print(''.join(reversed('mom')))
-h
+
+"""
+💡 Second attempt : Lets mimick the first step but remove the punctuation marks
+"""
+
+def is_palindrome_two(str):
+    c = reversed(str)
+    a = ''.join(ch for ch in c if ch.isalnum())
+    d = ''.join(ch for ch in str if ch.isalnum())
+    print(a)
+    if a.lower() == d.lower():
+        return True
+    else:
+        return False
+
+
+print(is_palindrome_two("Mr. Owl ate my metal worm"))
