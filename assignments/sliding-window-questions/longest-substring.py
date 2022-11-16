cbec89124a67e0cd57f5585581112f0d9f80ae91
Linda Joy <wawirajoy98@gmail.com>
2022-11-16 20:31:31 +0300

    📝 Added proper comments and reference links

---
+++ assignments/sliding-window-questions/longest-substring.py
@@ -0,0 +1,30 @@
+"""
+Link to question: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+Given a string s, find the length of the longest substring without repeating characters.
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Difference between a subsequence and a substring:
+
+1) A substring - a contiguous(continuous) sequence of characters present within a string. 
+                   It is a string present inside a string.
+
+2) A subsequence  - is a sequence that can be derived from another sequence of elements 
+                    without changing the order of the remaining elements
+
+For Example: {A, B, D} is one of the subsequences of the sequence {A, B, C, D, E} obtained after removing {C} and {E}.
+
+For reference: https://www.codingninjas.com/codestudio/library/subsequence-vs-substring
+
+"""
+
+# Solution 1: Naive approach(Brute Force Algorithms)
+
+print('hello World')
