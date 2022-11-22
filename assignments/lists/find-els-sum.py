54d36bf193b22e9afcc44004af3ce2278362feef
Linda Joy <wawirajoy98@gmail.com>
2022-11-22 21:56:32 +0300

    🧮 Added proper calculation on finding the two sum problem

---
+++ assignments/lists/find-els-sum.py
@@ -0,0 +1,45 @@
+'''
+In this problem, you have to implement the find_sum(lst,k) 
+function which will take a number k as input and return two numbers that add up to k.
+
+Sample Input
+
+lst = [1,21,3,14,5,60,7,6]
+k = 81
+
+Sample Output
+
+lst = [21,60]
+
+'''
+
+def find_sum(lst, k):
+    # Initializes an empty dictionary
+    seen = {}
+
+    # Loops through the list
+    for el in lst:
+        print(k)
+        print(el)
+        diff = k - el
+        if seen.get(el):
+            return [el, diff]
+        seen[diff] = el
+    
+lst = [1,21,3,14,5,60,7,6]
+k = 81
+
+find_sum(lst,k)
+
+'''
+💡 Brute Force Algorithm
+'''
+
+def find_sum_brute(list, k):
+    for i in range(len(list)):
+        for j in range(len(list)):
+            # if(lst[i]+lst[j] is k and i is not j):
+            if(list[i]+list[j] is k and i is not j):
+                return [list[i],list[j]]
+
+print(find_sum_brute(lst,k))
