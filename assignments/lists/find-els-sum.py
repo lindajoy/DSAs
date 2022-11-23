1066d2a49a06700f6d6e0166d79ea8a6d8310178
Linda Joy <wawirajoy98@gmail.com>
2022-11-23 09:10:28 +0300

    🧮 Find sum for 2 elements to equate

--- assignments/lists/find-els-sum.py
+++ assignments/lists/find-els-sum.py
@@ -24,13 +24,17 @@ def find_sum(lst, k):
             return [el, diff]
         seen[diff] = el
     
-lst = [1,21,3,14,5,60,7,6]
+lst = [1,21,3,14,5,60,7,6,77]
 k = 81
 
 find_sum(lst,k)
 
 '''
 💡 Brute Force Algorithm
+Traversing through the whole array of size, for each element in the list, check if any elements add up to the target k.
+
+Since we are iterating through the whole array , n times in the worst case,
+therefore time complexity is O(n2)
 '''
 
 def find_sum_brute(list, k):
@@ -40,3 +44,46 @@ def find_sum_brute(list, k):
                 return [list[i],list[j]]
 
 print(find_sum_brute(lst,k))
+
+print(len(lst))
+print(len(lst) - 1)
+
+print('Hmm interesting')
+
+def binary_search(a, item):
+    left, right = 0, len(a) - 1
+    found = False
+    indexi = -1
+
+    while left <= right and not found:
+        # Find the median value => Use floor division to return a significant whole number.
+        mid =  left + right // 2
+        
+        if a[mid] == item:
+           indexi = mid
+           found = True
+
+        else:
+            if item < a[mid]:
+                right = mid - 1
+
+            else:
+                left = mid + 1
+
+    if found:
+        return indexi
+
+    else:
+        return -1
+
+def find_sum3(list,k):
+    list.sort()
+    for j in range(len(list)):
+        # find the difference in list through binary search
+        # return the only if we find an index
+        index = binary_search(lst, k -lst[j])
+        if index is not -1 and index is not j:
+            return [lst[j], k -lst[j]]
+
+print(find_sum3([1, 5, 3], 2))
+print(find_sum3([1, 2, 3, 4], 5))
