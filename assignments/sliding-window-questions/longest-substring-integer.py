bca1708ab44f5bcf8536bf18b0879134674814d7
Linda Joy <wawirajoy98@gmail.com>
2022-12-12 21:39:18 +0300

    ✏️ Added comments on question

---
+++ assignments/sliding-window-questions/longest-substring-integer.py
@@ -0,0 +1,23 @@
+'''
+Given a string s and an integer k, 
+return the length of the longest substring of s that contains at most k distinct characters
+
+🧐 Analysis
+
+𐃉 Distinct means different.
+𐃉 A substring - a contiguous(continuous) sequence of characters present within a string. 
+                   It is a string present inside a string.
+
+𐃉 Whenever you hear the word contingous thinking of the sliding window.
+
+👻 Example 1:
+   input = 'eceba' , k =2
+   Output: 3
+   Explanation: The substring 'ece' with length 3
+
+👻 Example 2:
+   input = 'aa' , k = 1
+   output: 2
+   Explanation: The substring 'aa' with length 2
+ 
+'''
