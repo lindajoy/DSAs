f9d198368b7d808a171bd7a9399203fa511317fb
Linda Joy <wawirajoy98@gmail.com>
2023-01-03 09:14:34 +0300

    ✏️ To be continued,(Bad version Part 1)

---
+++ 30th-Dec/first-bad-version.py
@@ -0,0 +1,41 @@
+"""
+You are a product manager and currently leading a team to develop a new product. 
+Unfortunately, the latest version of your product fails the quality check. 
+Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, 
+which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. 
+Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Link: https://leetcode.com/problems/first-bad-version/
+
+"""
+
+'''
+Typical Binary Search Problem
+
+# Start with the middle element:
+# If the target value is equal to the middle element of the array, then return the index of the middle element.
+# If not, then compare the middle element with the target value,
+# If the target value is greater than the number in the middle index, then pick the elements to the right of the middle index, and start with Step 1.
+# If the target value is less than the number in the middle index, then pick the elements to the left of the middle index, and start with Step 1.
+# When a match is found, return the index of the element matched.
+# If no match is found, then return -1
+'''
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+# class Solution(object):
+#     def firstBadVersion(self, n):
+#         """
+#         :type n: int
+#         :rtype: int
+#         """
+
+my_list = list(8)
+print(my_list)
