e0e6aea6e5fd53ecf4da1616d5d92fa2b7707f24
Linda Joy <wawirajoy98@gmail.com>
2023-02-03 06:29:31 +0300

    ✏️ Added condition  to filter out the string with containing non alphanumeric characters

--- assignments/strings/length-last-word.py
+++ assignments/strings/length-last-word.py
@@ -8,10 +8,14 @@ A word is a maximal substring consisting of non-space characters only.
 How do you remove spaces from a text?
 
 """
-random_sentence = "luffy is still joyboy"
+
+"""
+💡 Add check if the characters contain a number: Pop them
+"""
+random_sentence = "luffy is still joyboy23!ß"
 
 def lengthOfLongestString(random_sentence):
-   random_array = [len(i) for i in random_sentence.split()]
+   random_array = [len(i) for i in random_sentence.split() if i.isalnum()]
    print('😁 👀', random_array)
    return random_array[-1]
 
