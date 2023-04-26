ba9250fd1129456cfa7175e324453430d87ed752
Linda Joy <wawirajoy98@gmail.com>
2023-04-26 07:11:51 +0300

    📃Add commented

--- assignments/strings/unique-character.py
+++ assignments/strings/unique-character.py
@@ -19,6 +19,10 @@ Input here is a string, Output is the index of the non-repeating character
 
 '''
 Pseudocode here would be:
+ 1. Here basically we will need to loop through the entire string and generate key value pairs(index: value) =>  Using the enumerate function.
+ 2. Check if the count of the specific character is equal to 1
+ 3. Return it
+ 4. If no character has occured once return -1.
 
 '''
 txt = "I love apples, apple are my favorite fruit"
@@ -29,6 +33,7 @@ print(x)
 def find_first_unique_character(text:str):
     # Generate key-Value of the index of each charater.
     for i, c in enumerate(text):
+        # Counts the number of times a character appears in a string.
         if text.count(c) == 1:
             return i
 
