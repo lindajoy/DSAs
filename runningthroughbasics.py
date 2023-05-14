4509c9ef1075bca15e96ad118f359b8eec163cc8
Linda Joy <wawirajoy98@gmail.com>
2023-05-14 19:38:57 +0300

    👩🏾‍💻 Refresher on some python basics

---
+++ runningthroughbasics.py
@@ -0,0 +1,39 @@
+"""
+Converting a list to a string in Python
+"""
+
+x = ['h','e', 'l','l']
+
+c = ''.join(x)
+d = str(x)
+# print(c)
+print(d)
+
+"""
+🤩 Remove duplicates from a list using set method
+"""
+
+restraunts = ["KFC", "ChickenInn", "CooperIvy", 'Sankara', 'Sankara', "kfc"]
+
+print(list(set(restraunts)))
+
+"""
+Sorting a string
+"""
+
+some_string = "happy"
+some_string_2 = ''.join(sorted(some_string))
+print(some_string_2)
+
+
+"""
+Enumerating using list comprehensions
+"""
+
+some_list = ["Firefly Lane", "Harry Potter", "A star is born", "Lord of Rings"]
+
+enumerate_list = [(i, num) for i, num in enumerate(some_list)]
+
+enumerate_list.sort(key=lambda x: x[1])
+
+print(enumerate_list)
