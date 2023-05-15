23ceb062f588b9868d42b97f4cedcb21fbd8913c
Linda Joy <wawirajoy98@gmail.com>
2023-05-15 11:54:36 +0300

    🧠 Brain Power dynamic solution

--- assignments/merge-intervals/brainpower.py
+++ assignments/merge-intervals/brainpower.py
@@ -15,10 +15,15 @@ Return the maximum points you can earn for the exam.
 
 Input: questions = [[3,2],[4,3],[4,4],[2,5]]
 Output: 5
+
+Leetcode question: 
 """
 
 def mostPoints(questions):
     maximumPoints = 0
+
+    if len(questions) == 1:
+        return questions[0][0]
 
     for i in range(len(questions)):
         total_sum = 0
@@ -42,6 +47,30 @@ x = [[3,2],[4,3],[4,4],[2,5]]
 y = [[1,1],[2,2],[3,3],[4,4],[5,5]]
 z = [[12,46],[78,19],[63,15],[79,62],[13,10]]
 
-print(mostPoints(z))
-print(mostPoints(y))
-print(mostPoints(x))
+# print(mostPoints(z))
+# print(mostPoints(y))
+# print(mostPoints(x))
+
+"""
+Dynamic Programming => Caching Method
+"""
+
+def mostPoints2(questions):
+    # We need to have an empty dictionary which acts as our cache
+    dfs = {}
+
+    def dfs(i):
+        if i >= len(questions):
+            return 0
+
+        if i in dfs:
+            return dfs[i]
+
+        print(max(dfs(i + 1), dfs(questions[0][0] + questions[i][1] + (i + 1))))
+        
+        dfs[i] = max(dfs(i + 1), dfs(questions[0][0] + questions[i][1] + (i + 1)))
+
+        return dfs[i]
+
+
+print(mostPoints2(z))
