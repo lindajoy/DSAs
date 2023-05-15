4000f110b8dd7846e857b21e9b7fb7d1dbbb5f3d
Linda Joy <wawirajoy98@gmail.com>
2023-05-15 12:07:47 +0300

    ✏️ Spacing

--- assignments/merge-intervals/ingredientsCosts.py
+++ assignments/merge-intervals/ingredientsCosts.py
@@ -4,7 +4,7 @@
 # computeCost(["bread","meat","bread"], [("bread",1),("meat",2)]) => 4
 
 """
-
+ 
 costs = [["bread",1], ["tomatoes",2], ["cheese",4], ["meat",5] ] 
   
 burger = [ "bread","meat","cheese","tomatoes","bread"]
@@ -12,7 +12,8 @@ burger = [ "bread","meat","cheese","tomatoes","bread"]
 def totalCostOfIngredients(cost, recipe):
     dict_costs = dict(cost) # Convert the costs list to a dictionary
 
-    total_cost =0
+    total_cost = 0 # Initialize the total cost to zero.
+    
     for i in recipe:
         if i in dict_costs:
             total_cost += dict_costs[i]
