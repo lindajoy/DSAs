8c092f02823046cbc1666662af819c29b25b21fd
Linda Joy <wawirajoy98@gmail.com>
2023-06-05 07:02:22 +0300

    💡 Depth First Search

---
+++ Graph-Theory/capture-lands.py
@@ -0,0 +1,37 @@
+"""
+Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+"""
+
+def captureRegions(matrix):
+
+    rows, columns = len(matrix), len(matrix[0])
+
+    # Capture the Unsurrounded region
+    def capture(row, column):
+        # Base Case is to check whether the row and column are within the matrixes range
+        if row < 0 or column < 0 or row == rows or column == columns or matrix[row][column] == "X":
+            return
+        
+        # Also marks this as visited
+        matrix[row][column] = "T"
+        capture(row + 1, column)
+        capture(row - 1, column)
+        capture(row, column - 1)
+        capture(row, column + 1)
+
+    # capture the surrounded region
+    for r in range(rows):
+        for c in range(columns):
+            if matrix[r][c] == "0":
+                matrix[r][c] = "X"
+
+    # Capture the unsurrounded region
+
+    for r in range(rows):
+        for c in range(columns):
+            if matrix[r][c] == "T":
+                matrix[r][c] = "0"
+
+    
---
+++ Graph-Theory/detonate-bombs.py
@@ -0,0 +1,52 @@
+"""
+You are given a list of bombs. The range of a bomb is defined as the area where its effect can be felt. 
+This area is in the shape of a circle with the center as the location of the bomb.
+
+The bombs are represented by a 0-indexed 2D integer array bombs where bombs[i] = [xi, yi, ri].
+ xi and yi denote the X-coordinate and Y-coordinate of the location of the ith bomb, whereas ri denotes the radius of its range.
+
+You may choose to detonate a single bomb. When a bomb is detonated, it will detonate all bombs that lie in its range. 
+These bombs will further detonate the bombs that lie in their ranges.
+
+Given the list of bombs, return the maximum number of bombs that can be detonated if you are allowed to detonate only one bomb.
+
+TIP: THIS IS A GRAPH THEORY QUESTION: BREADTH FIRST SEARCH OR DEPTH FIRST SEARCH ARE BOTH APPLICABLE ON THIS SOLUTION.
+
+Bombs: Here you are given the list of bombs
+"""
+
+from collections import defaultdict
+
+def detonateMaximumBombs(bombs):
+    # Create an adjacency list: Creating a graph on our own
+    adjList = defaultdict(list)
+
+    # Loop through the list of bombs
+    for i in range(len(bombs)):
+        for r in range(i + 1, len(bombs)):
+
+            xi, yi, ri = bombs[i]
+            xj, yj, rj = bombs[r]
+
+            distance = sqrt((xi - xj) ** 2 + (yi -yj) ** 2)
+
+            # Not understood this bit
+            if distance <= ri:
+                adjList[i].append[r]
+            if distance <= rj:
+                adjList[r].append[i]
+
+    def dfs(i, visit):
+        if i in visit:
+            return 
+        
+        for neighbour in adjList[i]:
+            dfs(neighbour,visit)
+
+        return len(visit)
+
+    res = 0
+    for i in range(bombs):
+        dfs(i, set())
+        res = max(res, dfs(i, set))
+        return res
