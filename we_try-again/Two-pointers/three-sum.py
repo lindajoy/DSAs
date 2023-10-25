eb8e2a05ebbdaae08ace838070cb2c050ffd9a2b
lindajoy <wawirajoy98@gmail.com>
2023-10-25 09:57:09 +0300

    ⏰ 😱 Spent a lot of time on this one(Three sum)
    Things I forgot were:
    The use of sets, Appending/ adding multiple elements in a list and set.
    Didn't pay attention to the last requirement of the question..

--- we_try-again/Two-pointers/three-sum.py
+++ we_try-again/Two-pointers/three-sum.py
@@ -37,9 +37,13 @@ my_big_list.append(my_other_list)
 print('I have added another list', my_big_list)
 
 my_triplet_list = [0,0,0,0,0,0]
-
+numsm = [-1,0,1,2,-1,-4]
+my_list_two = [-1,0,1,0]
+'''
+Wrong Answer => Does not return distinct triplets array; Its repetitive.
+'''
 def sum_of_three(nums):
-    distinct_triplets = []
+    distinct_triplets = set()
     sorted_array = sorted(nums)
     right = 0
     next_to_right = right + 1
@@ -47,7 +51,7 @@ def sum_of_three(nums):
 
     while right < left:
         if sorted_array[right] + sorted_array[left] + sorted_array[next_to_right] == 0:
-            distinct_triplets.append([sorted_array[right], sorted_array[left] ,sorted_array[next_to_right]])
+            distinct_triplets.add((sorted_array[right], sorted_array[left] ,sorted_array[next_to_right]))
             right += 1
             next_to_right += 1
             left -= 1
@@ -57,6 +61,81 @@ def sum_of_three(nums):
             right += 1
             next_to_right += 1
 
-    return distinct_triplets
+    return set(distinct_triplets)
 
-print(sum_of_three(my_triplet_list))
+# print(sum_of_three(my_triplet_list))
+# print(sum_of_three(numsm))
+# print(sum_of_three(my_list_two))
+
+'''
+Wrong  Answer => Again; Analysis and fix what did not work in the previous code
+'''
+
+def sum_of_three_improved(nums):
+    distinct_triplets = set()
+    sorted_array = sorted(nums)
+
+    for i in range(len(sorted_array) - 2):
+        print("The value of I at this particular point", i)
+        if i > 0 and sorted_array[i] == sorted[i - 1]:
+            continue
+
+        left, right = i + 1, len(sorted_array) - 1
+
+        while left < right:
+            total = sorted_array[i] + sorted_array[left] + sorted_array[right]
+
+            if total == 0:
+                distinct_triplets.add((sorted_array[i], sorted_array[left],sorted_array[right]))
+
+                # skip duplicate to avoid duplicate triplets
+                while left < right and sorted_array[left] == sorted_array[left + 1]:
+                    left += 1
+                while left < right and sorted_array[right] == sorted_array[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+
+            elif total < 0:
+                left += 1
+            else:
+                right -= 1
+
+        return distinct_triplets
+    
+# print(sum_of_three_improved(my_triplet_list))
+# print(sum_of_three_improved(numsm))
+
+
+# Let try again:
+
+def threeSum(nums):
+    res = set()
+    sorted_nums = sorted(nums)
+    print(sorted_nums)
+
+    for i , a in enumerate(sorted_nums):
+       # Enumerate generates index, and the value(belonging to that index)
+       if i > 0 and a == [i - 1]:
+           
+           continue
+       
+       left, right = i + 1, len(sorted_nums) - 1
+
+       while left < right:
+           potentialSum = a + sorted_nums[left] + sorted_nums[right]
+
+           if potentialSum > 0:
+               right -= 1
+           elif potentialSum < 0:
+               left += 1
+           else:
+               res.add((a, sorted_nums[left] ,sorted_nums[right]))
+               left += 1
+               while sorted_nums[left] == nums[left - 1] and left < right:
+                   left += 1
+    return res
+
+print(threeSum(numsm))
+print(threeSum([-1,0,1,0]))
