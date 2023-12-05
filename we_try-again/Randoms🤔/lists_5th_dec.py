2276b4159da35d5d4a578b759acf00b9bcb8a445
lindajoy <wawirajoy98@gmail.com>
2023-12-05 13:52:32 +0300

    🤟🏼: Linked lists: Using divmod operator

---
+++ we_try-again/Randoms🤔/lists_5th_dec.py
@@ -0,0 +1,15 @@
+list = [1, 2, 3, 4, 5]
+list.reverse()
+print(list)
+
+# Forgot this 🤔🤣 
+random_str = "helloWorld"
+reversed_string = random_str[::-1]
+print(reversed_string)
+
+string_random = 'helloworld'
+reversed_string = ''.join(reversed(string_random))
+print(reversed_string)
+
+list.pop(-2)
+print(list)
---
+++ we_try-again/linked_lists/happy_number.py
@@ -0,0 +1,64 @@
+"""
+Write an algorithm to determine if a number n is a happy number.
+We use the following process to check if a given number is a happy number:
+Starting with the given number n, replace the number with the sum of the squares of its digits.
+
+Repeat the process until:
+    =>The number equals 1, which will depict that the given number n is a happy number.
+    =>It enters a cycle, which will depict that the given number n is not a happy number.
+
+Return TRUE if n is a happy number, and FALSE if not.
+"""
+
+
+def isHappyNumber(n):
+    slow = n 
+    fast = sumOfSquared(n)
+
+    while fast != slow:
+        if fast == 1:
+            return True
+        else:
+            slow = sumOfSquared(n)
+            fast = sumOfSquared(fast)
+    return False
+
+def sumOfSquared(n:int) -> int:
+    output = 0
+
+    # 💡 Recursive way
+    # while n:
+    #     digit = n % 10
+    #     digit = digit **2
+    #     output += digit
+    #     n = n // 10
+    # return output
+
+    # USING DIVMOD
+    ans = divmod(n, 10)
+    for i in ans:
+        output += i ** 2
+    return output
+
+    
+print(isHappyNumber(23))
+
+s = b = 23
+
+while s == b:
+    s = 45
+
+# print(s)
+# print(b)
+# print(id(s))
+# print(id(b))
+
+def sumOfSquared2(n:int) -> int:
+    output = 0
+    s = divmod(n, 10)
+    for i in s:
+        output += i ** 2
+  
+    return output
+
+print(sumOfSquared2(12))
