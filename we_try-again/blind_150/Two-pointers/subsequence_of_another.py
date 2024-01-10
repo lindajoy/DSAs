b40ef90bf5ab0e56318ba696a7b3d5e26347d02b
lindajoy <wawirajoy98@gmail.com>
2024-01-10 11:28:19 +0300

    🧮: Validate subsequence

---
+++ we_try-again/blind_150/Two-pointers/subsequence_of_another.py
@@ -0,0 +1,33 @@
+"""
+Given two non-empty arrays of integers, 
+our goal is to write a function that establishes whether the second array is a valid subsequence of the first array.
+A valid subsequence of an array is classified as a set of numbers that aren’t necessarily adjacent in the array but are in the same order as they appear in the array. 
+For example, [2, 5, 6] and [2, 6] are both considered as valid subsequences of the array [2, 3, 4, 5, 6]. 
+It is also important to note that [2] and [2, 3, 4, 5, 6] are both valid subsequences of the array.
+
+Sample:
+
+Input array → [3, 6, 23, 7, 2, 4]
+
+Sequence → [3, 7, 4]
+
+Output → True
+"""
+
+def isValidSubsequence(array, sequence):
+    seq_ptr = 0
+    for i in range(len(array)):
+        if seq_ptr == len(sequence):
+            break
+        if sequence[seq_ptr] == array[i]:
+            seq_ptr += 1
+
+    return seq_ptr  == len(sequence)
+     
+array1 = [3, 6, 23, 7, 2, 4]
+Sequence1 = [3, 7, 4]
+array2 = [1, 2, 3, 4, 5]
+Sequence2 =[1, 3, 5]
+array3= [10, 20, 30, 40, 50]
+Sequence4 = [20, 40]
+print(isValidSubsequence(array3, Sequence4))
