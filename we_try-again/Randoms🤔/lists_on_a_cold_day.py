867642dc2c18d52f3fe88e35ededeb43bbd6d4c6
lindajoy <wawirajoy98@gmail.com>
2024-01-12 07:47:16 +0300

    Lists 101: This is a fun morning because of my playlist maybe 🎶

--- we_try-again/Randoms🤔/lists_on_a_cold_day.py
+++ we_try-again/Randoms🤔/lists_on_a_cold_day.py
@@ -17,3 +17,32 @@ print('Here are my new names', new_names)
 new_new_names = list(names)
 new_new_names.append("Wawira")
 print('Here are my new new names', new_new_names)
+
+# 💡 Remove function => Remove first occurence of element from list.
+new_new_names.remove('Wawira')
+print("Did i remove 'Wawira'?", new_new_names)
+
+# If value does not exist python, returns a valueexpection error!
+
+# Insert, takes in index where you want to insert and the element you want to insert.
+new_names.insert(0,'Ann')
+print('😮‍💨 Added Ann at index at index 0:',new_names)
+
+# Index: Returns the first index of an element.
+print('Should expect 0:', new_names.index('Ann'))
+# Index => can also take in index to start (Startindex) when looking for an element
+print("Should expect 3:", new_names.index("Ann", 2))
+# Note to self: This is a very good morning!
+
+# Okay lets understand pop() method
+# Removes element at a specific index.
+print(new_names.pop())
+print(new_names)
+
+print(new_names.pop(4))
+print(new_names)
+
+print(new_names.pop(3))
+print(new_names)
+
+
