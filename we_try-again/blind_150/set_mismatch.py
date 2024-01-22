e0888cf8197f457e95470b4f43a6d52331f8c120
lindajoy <wawirajoy98@gmail.com>
2024-01-22 19:52:33 +0300

    🕵🏿: Interesting facts about creating a dictionary using counter or empty dictionary

--- we_try-again/blind_150/set_mismatch.py
+++ we_try-again/blind_150/set_mismatch.py
@@ -13,6 +13,12 @@ You are given an integer array nums representing the data status of this set aft
 Find the number that occurs twice and the number that is missing and return them in the form of an array.
 """
 
+"""
+QUESTION FOR INTERVIEWER:
+    1. When you say repitition we always mean that the number will always be repeated twice, not thrice?
+    2. 
+"""
+
 from collections import Counter
 
 def setMisMatch(nums):
@@ -52,4 +58,52 @@ def setMisMatch2(nums):
     return res
 
 print(setMisMatch2([1,2,2,4]))
+
+# Lets try something
+numbers = [1,2,3,5,5,7,6,9]
+count_of_number = Counter(numbers)
+print("Count of numbers", count_of_number)
+list_zeros = []
+for i in range(1, len(numbers) + 1):
+    if count_of_number[i] == 0:
+        list_zeros.append(i)
+
+print(list_zeros) 
+
+# Random lets create a dictionary manually
+random_count = {}
+list_zeros_2 = []
+
+for num in numbers:
+    random_count[num] = random_count.get(num, 0) + 1
+
+print('Created Dictionary Manually', random_count)
+for i in range(1, len(numbers) + 1):
+    if i not in random_count:
+        list_zeros_2.append(i)
+
+print(list_zeros_2)
+
+
+# Last example on dictionaries.
+random_count_2 = {}
+list_zeros_3 = []
+
+for num in numbers:
+    if num in random_count_2:
+        random_count_2[num] += 1
+    else:
+        random_count_2[num] = 1
+
+for i in range(1, len(numbers) + 1):
+    if i not in random_count_2:
+        list_zeros_3.append(i)
+
+print('My random count:',random_count_2)
+print('My random count:',list_zeros_3)
+
+
+
+
+
 
