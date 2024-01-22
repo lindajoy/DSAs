cd760a560eae51a6c2c0a5c5e9599d04c112b0d2
lindajoy <wawirajoy98@gmail.com>
2024-01-22 15:41:19 +0300

    🤦🏽‍♀️Realized first solution was wrong

--- we_try-again/blind_150/one_edit.py
+++ we_try-again/blind_150/one_edit.py
@@ -12,18 +12,29 @@ def oneEdit(string1, string2):
     while string1Ptr < len(string1) and string2Ptr < len(string2):
         if string1[string1Ptr] != string2[string2Ptr]:
             count += 1
-            if len(string2) > len(string1):
+            if count > 1:
+                return False  # More than one difference found
+
+            # Move the pointer of the longer string
+            if len(string1) > len(string2):
+                string1Ptr += 1
+            elif len(string2) > len(string1):
                 string2Ptr += 1
             else:
+                # If lengths are equal, move both pointers
                 string1Ptr += 1
+                string2Ptr += 1
+        else:
+            string1Ptr += 1
+            string2Ptr += 1
+
+    # Check if there is a remaining character in either string
+    if string1Ptr < len(string1) or string2Ptr < len(string2):
+        count += 1
         
-
-        string1Ptr += 1
-        string2Ptr += 1
-
     return True if count == 1 else False
 
-print(oneEdit("apple", "pple"))
+print(oneEdit("abcde", "abcd"))
 print(oneEdit("hello", "hello"))
 print(oneEdit("rain", "ran"))
 print(oneEdit(" ", " "))
