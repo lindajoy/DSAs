345659fc3ed63e1879143a4c3a7aecb7f6703937
lindajoy <wawirajoy98@gmail.com>
2024-02-02 15:32:21 +0300

    Dictionaries and happy numbers

---
+++ basics/dictionaries_basics.py
@@ -0,0 +1,21 @@
+my_dictionary = {
+    "apple": 10,
+    "banana": 5,
+    "orange": 7,
+    "grape": 12,
+    "kiwi": 8
+}
+
+        # char_count[char] = char_count.get(char, 0) + 1
+# With Python we always have to assign a value, to a particular property we want to change.
+
+my_dictionary["apple"] = my_dictionary.get("apple", 0) + 1
+print(my_dictionary)
+
+for i in my_dictionary.keys():
+    my_dictionary[i] = my_dictionary.get(i, 0) +10
+
+print("Added by 10", my_dictionary)
+
+my_dictionary["raspberry"] = my_dictionary.get("raspberry", 0) + 1
+print('Added raspberry:', my_dictionary)
