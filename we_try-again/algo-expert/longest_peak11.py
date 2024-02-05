f33186b1a4230c4f67e0c79a4b2ae3ed06947716
lindajoy <wawirajoy98@gmail.com>
2024-02-05 14:41:05 +0300

    Longest Peak 11

---
+++ we_try-again/algo-expert/longest_peak11.py
@@ -0,0 +1,24 @@
+def longestMountain(arr):
+    ans = 0
+    i = 0
+
+    for i in range(len(arr) - 1):
+        while i + 1 < len(arr) and arr[i] == arr[i+1]:
+            i+=1
+        increasing = 0
+        decreasing = 0
+
+        while i + 1 < len(arr) and arr[i] < arr[i + 1]:
+            increasing += 1
+            i += 1
+
+        while i + 1 < len(arr) and arr[i] > arr[i + 1]:
+            decreasing += 1
+            i += 1
+
+        if increasing > 0 and decreasing > 0:
+            ans = max(ans, increasing + decreasing + 1)
+
+    return ans
+
+print(longestMountain([2,1,4,7,3,2,5]))
