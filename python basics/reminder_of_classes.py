2999b258254cb99c3c199d2d879864c436e09b00
lindajoy <wawirajoy98@gmail.com>
2024-02-10 15:50:37 +0300

    Refreshing on some knowledge

--- python basics/reminder_of_classes.py
+++ python basics/reminder_of_classes.py
@@ -18,9 +18,30 @@ class Person(object):
         self.name = renamed
         print("Now my name is {0}".format(self.name))
 
+    def product(self, age):
+        return int(age) * 2
+
 
 
 Linda = Person('Wawira')   
 
 print(Linda.age)
 print(Linda.name)
+print(Linda.product(400))
+
+
+print('{0} {1} {2}'.format('Joy', 'Lidah', 'Wawira'))
+
+# Simplest way of testing whether a string is a palindrome.
+my_sample_string = "ababa"
+print('In the case of `ababa` should return a positive outcome:', my_sample_string == my_sample_string[::-1])
+
+# Here is a way of reversing a string: my_sample_string[::-1]
+# What about if i wanted to use the reversed instead?
+my_name = "Joy L Wawira"
+reverse = ''.join(reversed(my_name))
+print(reverse)
+random_list = [40, 50, 60, 70]
+print(random_list.reverse())
+
+# Hmm: In conclusion, always use this [::-1] to reverse a list or string
