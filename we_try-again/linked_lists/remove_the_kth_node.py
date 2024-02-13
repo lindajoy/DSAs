a5647ac20d58b55f7fb21ee5a01b2574d4bf8772
lindajoy <wawirajoy98@gmail.com>
2024-02-13 12:02:24 +0300

    Added steps to solve the question

--- we_try-again/linked_lists/remove_the_kth_node.py
+++ we_try-again/linked_lists/remove_the_kth_node.py
@@ -28,6 +28,24 @@ Each LinkedList node has an integer value as well as a next node pointing to the
 # The 4th node from the end of the list (the node with value 6) is removed.
 # 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 7 -> 8 -> 9
 
+# Solution Idea
+
+"""
+The idea here is to use two pointers to find the (N + 1)th from the end.
+In the beginning, the first pointer is at the (N + 1)th node from the start, and the second painter is at the head of linked list.
+By moving both pointers one step at a time untill the forst point reaches the end, the second pointer will point to the (N + 1)th node from the end, 
+and we easily remove the next node, which is the Nth node from the list end.
+"""
+
+# SOLUTION STEPS
+"""
+    1. Initialize two pointers: FirstPointer and Second Pointer.
+       Initially, the second pointer is positioned at the first node of the linked list, and the firstPointer is set to the (N + 1)th node from the beginning.
+    
+    2. Now, both pointers maintain a constant gap of N nodes. We advance them together untill the firstPointer reaches the lastNode.
+    3. At this point, the secondPointer points to the (N + 1) th node from the end.
+        To remove the Nth node from the end of the list, we update the next pointer of the secondPointer to the point to the next node's next pointer.
+"""
 # I always like initializing the listNode Class
 
 class ListNode:
@@ -36,6 +54,7 @@ class ListNode:
         self.value = value
 
 # Solution using Two pointers; Fast and slow pointers solution.
+
 
 def removeNthNodeFromEnd(head: ListNode, n: int) -> ListNode:
     # Initialize the slow and fast pointer
