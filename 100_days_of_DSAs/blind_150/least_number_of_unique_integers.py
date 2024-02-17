1a8b3d3b2e44bd3c22d194f57e3c7733e7d279f4
lindajoy <wawirajoy98@gmail.com>
2024-02-17 14:52:01 +0300

    Fixed errors on our first implementation

--- 100_days_of_DSAs/blind_150/least_number_of_unique_integers.py
+++ 100_days_of_DSAs/blind_150/least_number_of_unique_integers.py
@@ -37,3 +37,42 @@ def leastNumberOfUniqueIntegers(arr, k):
     return len(x)
 
 print(leastNumberOfUniqueIntegers([4,3,1,1,3,3,2], 3))
+
+# 💡 Using a heap;
+
+# Lets talk about heaps kidogo..
+
+"""
+A heap is a specialized binary tree. 
+
+We use heaps when all we care about is the largest or the smallest elements , and you do not need to support fast lookcup, delete , 
+or search oparations for arbitary elements.
+
+A heap is good choice when you need to compute the k largest or k smallest in a collection
+
+"""
+
+import heapq
+from collections import Counter
+
+def return_unique_elements_from_array(arr, k):
+    # Here we are creating a dictionary with a counter that counts every account;
+    # and store them in a dictionary
+    count_dict = Counter(arr)
+    # Converts the freq_list to a list
+    freq_list = list(count_dict.values())
+    # Here we are converting our list to a heap
+    heapq.heapify(freq_list)
+
+    # We use len of heap to help us output the end result
+    res = len(freq_list)
+
+    while k > 0 and freq_list:
+        # Pops out the smallest element.
+        value = heapq.heappop(freq_list)
+        if k >= value:
+            k = k - value
+            res -= 1
+
+    return res
+print(return_unique_elements_from_array([4,3,1,1,3,3,2], 3))
