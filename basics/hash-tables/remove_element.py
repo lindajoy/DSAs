2a429fb4a2f58ef32e906857e5d315c27a75bafd
lindajoy <wawirajoy98@gmail.com>
2024-02-22 16:19:00 +0300

    Remove element

---
+++ basics/hash-tables/remove_element.py
@@ -0,0 +1,34 @@
+"""
+REMOVE ELEMENT
+
+Given an integer array nums and an integer val, 
+remove all occurrences of val in nums in-place. 
+The order of the elements may be changed. 
+Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, 
+to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+The remaining elements of nums are not important as well as the size of nums.
+Return k.
+
+"""
+
+# What the question is asking above is what will be the length of the array if i remove all the ks from my array.
+
+def remove_element(arr, k):
+    n = len(arr)
+    i = 0
+
+    while i < n:
+        if arr[i] == k:
+            arr[i], arr[n - 1] = arr[n - 1], arr[i]
+            n -= 1  # decrement the length of the array by discarding the last element
+        else:
+            i += 1
+    
+    return n
+
+print(remove_element([3,2,2,3], 3))
+print(remove_element([0,1,2,2,3,0,4,2],2))
