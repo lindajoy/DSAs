51384c38f33abebd684ba01180ee4ad5e9cf63a9
lindajoy <wawirajoy98@gmail.com>
2024-03-01 11:50:20 +0300

    Maximum odd binary number

---
+++ 100_days_of_DSAs/string_number_questions/maximum_odd_number.py
@@ -0,0 +1,44 @@
+# Maximum Odd Binary Number.
+
+"""
+You are given a binary string s that contains at least one '1'.
+
+You have to rearrange the bits in such a way that the resulting binary number 
+is the maximum odd binary number that can be created from this combination.
+
+Return a string representing the maximum odd binary number that can be created from the given combination.
+
+Note that the resulting string can have leading zeros.
+
+Example 1:
+
+Input: s = "010"
+Output: "001"
+Explanation: Because there is just one '1', it must be in the last position. So the answer is "001".
+Example 2:
+
+Input: s = "0101"
+Output: "1001"
+Explanation: One of the '1's must be in the last position. The maximum number that can be made with the remaining digits is "100". So the answer is "1001".
+"""
+
+# Cant believe this question was testing on string concatenation
+
+# One thing thats for sure is that for us to achieve an odd number whenever we are given a binary number: 
+# We need to add 1 to our last Index
+
+# The question assures us that there will always be one 1.
+
+# String Concatenation.
+def maximumOddBinaryNumber(binary):
+    count_of_ones = 0
+
+    for i in binary:
+        if i == "1":
+            count_of_ones += 1
+
+    return (count_of_ones - 1) * "1" + (len(binary) - count_of_ones) * "0" + "1"
+
+
+
+
